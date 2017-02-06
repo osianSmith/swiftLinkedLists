e8c43c31af4305ae281b464fa69188738ecdd854
Osian Smith <osiansmith@me.com>
2017-02-06 13:31:47 +0000

    Update main.swift
    Removes playground

--- LinkedLists/main.swift
+++ LinkedLists/main.swift
@@ -1,12 +1,1 @@
-//
-//  main.swift
-//  LinkedLists
-//
-//  Created by Osian on 21/01/2017.
-//  Copyright © 2017 Osian Smith. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
 
